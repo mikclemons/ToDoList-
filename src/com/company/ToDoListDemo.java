fd83bc9e2507f49e8cc0e945d6ab5c8981f03914
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-28 19:37:46 -0500

    Create accessor methods for the ToDoListItem class.

--- src/com/company/ToDoListDemo.java
+++ src/com/company/ToDoListDemo.java
@@ -1,8 +1,10 @@
 package com.company;
+import java.util.Scanner;
 
 public class ToDoListDemo {
 
     public static void main(String[] args) {
-	
+
+
     }
 }
--- src/com/company/ToDoListItem.java
+++ src/com/company/ToDoListItem.java
@@ -2,4 +2,28 @@ package com.company;
 
 public class ToDoListItem {
 
+    private String description;
+    private String state;
+
+    public ToDoListItem(String description, String state){
+        this.description = description;
+        this.state = state;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
 }
