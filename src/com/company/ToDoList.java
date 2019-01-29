6290d7415cfa4947e88e167f0a603583516b7128
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-29 13:52:55 -0500

    empty message

--- src/com/company/ToDoList.java
+++ src/com/company/ToDoList.java
@@ -4,28 +4,31 @@ public class ToDoList {
 
     private ToDoListItem[] toDoListItems = new ToDoListItem[3];
 
-    public void addItem(ToDoListItem item){
-        for(int i = 0; i < toDoListItems.length; i ++ ){
-            if(toDoListItems[i] == null){
+    public void addItem(ToDoListItem item) {
+        for (int i = 0; i < toDoListItems.length; i++) {
+            if (toDoListItems[i] == null) {
                 toDoListItems[i] = item;
                 break;
             }
         }
     }
 
-    public void deleteItem(){
-        for(int i = toDoListItems.length - 1; i < -1; i ++){
-            if(toDoListItems[i] != null){
+    public void deleteItem() {
+        for (int i = toDoListItems.length - 1; i < -1; i++) {
+            if (toDoListItems[i] != null) {
                 toDoListItems = null;
             }
         }
     }
 
-    public ToDoListItem[] getItems(){
+    public ToDoListItem[] getItems() {
         return this.toDoListItems;
     }
 
-    public ToDoListItem[] getItemsByState(){
-
+    public ToDoListItem[] getItemsByState() {
+        for (ToDoListItem item : toDoListItems) {
+            if(ToDoListItem.getState().equals())
+        }
     }
+  }
 }
